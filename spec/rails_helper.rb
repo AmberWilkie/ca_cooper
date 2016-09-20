7c3f2876672092392d2a13f3ad249e30d25fcbfe
Amber <amber@amberwilkie.com>
2016-09-20 11:04:59 +0200

    Add json parser module

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,17 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
 require 'database_cleaner'
 ActiveRecord::Migration.maintain_test_schema!
 
+
 RSpec.configure do |config|
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  config.include FactoryGirl::Syntax::Methods
+  config.include ResponseJSON
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!

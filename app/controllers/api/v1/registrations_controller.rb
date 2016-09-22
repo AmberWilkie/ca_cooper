6c771632170c2b9f32e0446f8faf019a4298ea69
Amber <amber@amberwilkie.com>
2016-09-22 15:45:15 +0200

    Sanitize 'name' for user reg update

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -4,8 +4,14 @@ class Api::V1::RegistrationsController < DeviseTokenAuth::RegistrationsControlle
 
   # protect_from_forgery with: :null_session
   # respond_to :json
-  # 
+  #
   # def create
   #   super
   # end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
 end

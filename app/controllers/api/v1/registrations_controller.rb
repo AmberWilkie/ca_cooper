966711891e158c3d176fafe29e14549e68956667
Amber <amber@amberwilkie.com>
2016-09-22 16:54:52 +0200

    Attempt to fix errors coming from API post to registrations -13

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::RegistrationsController < DeviseTokenAuth::RegistrationsController
   before_action :account_update_params
-  protect_from_forgery with: :null_session
-  before_action :validate_account_update_params, only: :update
-  skip_before_action :verify_authenticity_token, only: :update
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  # before_action :validate_account_update_params, only: :update
+  # skip_before_action :verify_authenticity_token, only: :update
 
 
   protected
@@ -10,7 +10,7 @@ class Api::V1::RegistrationsController < DeviseTokenAuth::RegistrationsControlle
     params.permit(:name, :nickname, :email, :password, :password_confirmation, :current_password)
   end
 
-  def validate_account_update_params
-    super
-  end
+  # def validate_account_update_params
+  #   super
+  # end
 end

ed899df0deba38de56c5c3f1590bb112e43a25d1
Amber <amber@amberwilkie.com>
2016-09-22 16:04:28 +0200

    Attempt to fix errors coming from API post to registrations

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  prepend_before_action :configure_permitted_parameters
+  prepend_before_action :validate_account_update_params
   protect_from_forgery with: :null_session
 
 
   protected
-  def configure_permitted_parameters
+  def validate_account_update_params
     # devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name, :nickname) }
     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :name, :nickname])
   end

8b025362a9037762b06a3d7f6a7dedf5321e557b
Amber <amber@amberwilkie.com>
2016-09-22 15:38:08 +0200

    Update registrations controller route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     end
 
     namespace :v1, defaults: { format: :json } do
-      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks], controllers: { registration: '/api/v1/registration'}
+      mount_devise_token_auth_for 'User', controllers: { registrations: 'api/v1/registrations'},  at: 'auth', skip: [:omniauth_callbacks]
       resources :performance_data, only: [:create, :index]
     end
   end

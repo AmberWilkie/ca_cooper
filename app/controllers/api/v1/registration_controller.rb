1737384a6162575e76e8fb2916fd06f2955c3ade
Amber <amber@amberwilkie.com>
2016-09-22 15:31:03 +0200

    Change inheritance of registrations controller

--- app/controllers/api/v1/registration_controller.rb
+++ app/controllers/api/v1/registration_controller.rb
@@ -1,7 +1,8 @@
-class Api::V1::RegistrationsController < Devise::RegistrationsController
+class Api::V1::RegistrationsController < DeviseTokenAuth::RegistrationsController
   protect_from_forgery with: :null_session
   respond_to :json
 
   def create
+    super
   end
 end

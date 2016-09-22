f442c062b71c148b77e6135b7588137f17511da6
Amber <amber@amberwilkie.com>
2016-09-22 15:12:22 +0200

    Add controller reroute to routes.rb for registration update

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     end
 
     namespace :v1, defaults: { format: :json } do
-      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks], controllers: { registration: '/api/v1/registration'}
       resources :performance_data, only: [:create, :index]
     end
   end

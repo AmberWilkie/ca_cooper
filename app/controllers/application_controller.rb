28cd93e9659b262474e2e41d468368ca98bbbba1
Amber <amber@amberwilkie.com>
2016-09-22 17:15:08 +0200

    Remove code we added with proc business

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
+  protect_from_forgery with: :null_session
   respond_to :json
 
 

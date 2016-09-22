cee9ba104cc9d273dd1bc9d5260c98a03bc73b08
Amber <amber@amberwilkie.com>
2016-09-22 15:52:13 +0200

    Fix config params name -3

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,3 +1,9 @@
 class Api::V1::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  prepend_before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) << :name
+    devise_parameter_sanitizer.permit(:account_update) << :name
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   respond_to :json
 
-  prepend_before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) << :name
-    devise_parameter_sanitizer.permit(:account_update) << :name
-  end
 end
